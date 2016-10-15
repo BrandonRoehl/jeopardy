32e2fbc41be3447254c433a5d1c649d6b6c4dd9d
Brandon Roehl <BrandonRoehl@gmail.com>
2016-10-15 16:37:44 -0500

    Answer Revel

--- src/sample/JeopardyQuestionLaunch.java
+++ src/sample/JeopardyQuestionLaunch.java
@@ -3,7 +3,10 @@ package sample;
 import javafx.event.EventHandler;
 import javafx.event.ActionEvent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -12,16 +15,29 @@ import javafx.stage.Stage;
  */
 public class JeopardyQuestionLaunch implements EventHandler<ActionEvent> {
     private Stage stage;
+    private TextArea textArea;
+    private BorderPane bottom;
+
+    private final String question;
+    private final String answer;
 
     public JeopardyQuestionLaunch(Main main, int x, int y) {
+        question = "this is a test";
+        answer = "this is a test";
+
         BorderPane root = new BorderPane();
 
+        textArea = new TextArea("Question:\n" + question);
+        textArea.setEditable(false);
+        textArea.setFocusTraversable(false);
+        root.setCenter(textArea);
 
+        bottom = new BorderPane();
+        Button show = new Button("Revel Answer");
+        show.setOnAction(new Revel());
+        bottom.setCenter(show);
 
-
-
-
-
+        root.setBottom(bottom);
 
 
         Scene scene = new Scene(root, 300, 300);
@@ -34,5 +50,15 @@ public class JeopardyQuestionLaunch implements EventHandler<ActionEvent> {
     public void handle(ActionEvent event) {
         stage.show();
         stage.centerOnScreen();
+    }
+
+
+    private class Revel implements EventHandler<ActionEvent> {
+        @Override
+        public void handle(ActionEvent event) {
+            textArea.setText("Question:\n" + question + "\n\nAnswer:\n" + answer);
+            bottom.setCenter(null);
+
+        }
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,7 +14,7 @@ import javafx.stage.StageStyle;
 
 public class Main extends Application {
     private static final int PADDING = 5;
-    protected Button[][] buttons;
+    Button[][] buttons;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
