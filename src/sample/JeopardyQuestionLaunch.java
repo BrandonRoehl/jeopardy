d107b159865558c43ced661f7fd9d4a926cdb92d
Brandon Roehl <BrandonRoehl@gmail.com>
2016-10-15 16:09:33 -0500

    Moving on

--- src/sample/JeopardyQuestionLaunch.java
+++ src/sample/JeopardyQuestionLaunch.java
@@ -2,6 +2,8 @@ package sample;
 
 import javafx.event.EventHandler;
 import javafx.event.ActionEvent;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -9,17 +11,28 @@ import javafx.stage.Stage;
  * Created by BrandonRoehl on 10/15/16.
  */
 public class JeopardyQuestionLaunch implements EventHandler<ActionEvent> {
+    private Stage stage;
 
     public JeopardyQuestionLaunch(Main main, int x, int y) {
+        BorderPane root = new BorderPane();
 
+
+
+
+
+
+
+
+
+        Scene scene = new Scene(root, 300, 300);
+        stage = new Stage();
+        stage.setScene(scene);
+        stage.initModality(Modality.APPLICATION_MODAL);
     }
 
     @Override
     public void handle(ActionEvent event) {
-        Stage stage = new Stage();
-        stage.initModality(Modality.APPLICATION_MODAL);
-        //Fill stage with content
-        stage.showAndWait();
-        System.out.println("This worked");
+        stage.show();
+        stage.centerOnScreen();
     }
 }
